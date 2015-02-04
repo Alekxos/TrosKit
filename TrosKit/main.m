b54fbcd2a4bb311530afeef565007f95c6a507cf
Alex Boulton <alexboulton24@gmail.com>
2015-02-03 17:18:09 -0800

    Initial Commit

---
+++ TrosKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrosKit
+//
+//  Created by Alex Boulton on 2/3/15.
+//  Copyright (c) 2015 Alex Boulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TrosKitTests/TrosKitTests.m
@@ -0,0 +1,40 @@
+//
+//  TrosKitTests.m
+//  TrosKitTests
+//
+//  Created by Alex Boulton on 2/3/15.
+//  Copyright (c) 2015 Alex Boulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TrosKitTests : XCTestCase
+
+@end
+
+@implementation TrosKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
